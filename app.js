10443ec4db8cb38dd4b1e10abcb138cf81a1ae9a
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 18:27:00 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 // write code here
+const state1 = { username: 'john', point: 100, loading: true };
+
+const newStatenaja = Object.assign({}, state1, { loading: false, point: 75, success: true });
+
+console.log(newStatenaja);
